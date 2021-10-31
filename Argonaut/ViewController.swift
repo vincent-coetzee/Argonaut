3f4dc944d348e4594d41f57763f848889e3eef21
Vincent Coetzee <vincent.coetzee@icloud.com>
2021-10-31 11:18:13 +0200

    Initial Commit

---
+++ Argonaut/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Argonaut
+//
+//  Created by Vincent Coetzee on 31/10/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
